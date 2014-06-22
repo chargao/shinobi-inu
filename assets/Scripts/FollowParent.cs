6d99abcc2f1801b6983eedb470ab9f4ba48c34ce
PeanutofDeath <ans.ballard@gmail.com>
2014-06-21 20:02:41 -0500

    all the things

--- assets/Scripts/FollowParent.cs
+++ assets/Scripts/FollowParent.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class FollowParent : MonoBehaviour {
 
-	// 340, 11, 355
+	public GameObject ToFollow;
+
+	// 340, 19, 346
+	void Start() {
+		transform.rotation = new Quaternion (340.0f, 19.0f, 346.0f, transform.rotation.w);
+	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(transform.parent.position.y != transform.position.y && transform.parent)
-			transform.position = new Vector3(transform.position.x, transform.parent.position.y, -5.5f);
+		//if(transform.parent.position.y != transform.position.y && transform.parent)
+		transform.position = new Vector3(ToFollow.transform.position.x, ToFollow.transform.position.y-1.5f, ToFollow.transform.position.z-4.5f);
+		transform.localEulerAngles = new Vector3 (334.0f, 19.1f, 324.0f);
 	}
 }
